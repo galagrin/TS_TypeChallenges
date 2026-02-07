124818b969c50c90a0d7d0052f539ce5b3a56618
Galina Bobrik <galagrinko@gmail.com>
2026-02-07 16:32:08 +0400

    add replaceAll task

--- replace.ts
+++ replace.ts
@@ -1,6 +1,6 @@
 // Replace
 // 📝 Условие задачи
-// Реализуйте `Replace<S, From, To>`, который в строке S заменяет все вхождения From на To.
+// Реализуйте `Replace<S, From, To>`, который в строке S заменяет Первое вхождения From на To.
 
 // Пример
 //type replaced = Replace<'types are fun!', 'fun', 'awesome'> // expected to be 'types are awesome!'
@@ -9,4 +9,24 @@ type Replace<S extends string, From extends string, To extends string> = From ex
     ? S
     : S extends `${infer Left}${From}${infer Tail}`
     ? `${Left}${To}${Tail}`
+    : S;
+
+// Replace all
+// 📝 Условие задачи
+// Реализуйте `ReplaceAll<S, From, To>`, который в строке S заменяет Все вхождения From на To.
+
+//  Тесткейсы
+type Test = ReplaceAll<"foobar", "bar", "foo">; //'foofoo'
+type Test1 = ReplaceAll<"foobar", "bag", "foo">; //'foobar'
+type Test2 = ReplaceAll<"foobarbar", "bar", "foo">; //'foofoofoo'
+type Test3 = ReplaceAll<"t y p e s", " ", "">; //'types'
+type Test4 = ReplaceAll<"foobarbar", "", "foo">; //'foobarbar'
+type Test5 = ReplaceAll<"foobarfoobar", "ob", "b">; //'fobarfobar'
+type Test6 = ReplaceAll<"foboorfoboar", "bo", "b">; //'foborfobar'
+type Test7 = ReplaceAll<"", "", "">; //''
+
+type ReplaceAll<S extends string, From extends string, To extends string> = From extends `${""}`
+    ? S
+    : S extends `${infer Head}${From}${infer Tail}`
+    ? `${Head}${To}${ReplaceAll<Tail, From, To>}`
     : S;
